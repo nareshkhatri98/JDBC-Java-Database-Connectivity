195e77327e07a1e5ccd08be63614f6d49c80ee8d
Naresh Khatri <nareshkhatri9810@gmail.com>
2024-06-13 17:33:42 +0545

    Create a simple GUI using swing

--- src/FormOne.java
+++ src/FormOne.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.sql.*;
 
 public class FormOne {
     JFrame f;
@@ -8,26 +9,31 @@ public class FormOne {
     JButton btninsert;
 
     FormOne() {
-        f = new JFrame("Login form");
+        f = new JFrame("Login Form");
 
+        // For ID
         labelid = new JLabel("ID:");
         labelid.setBounds(50, 50, 100, 30);
         textfieldid = new JTextField();
-        textfieldid.setBounds(200, 50, 100, 30);
+        textfieldid.setBounds(200, 50, 150, 30);
 
+        // For Username
         labelusername = new JLabel("Username:");
         labelusername.setBounds(50, 100, 100, 30);
         textfieldusername = new JTextField();
-        textfieldusername.setBounds(200, 100, 100, 30);
+        textfieldusername.setBounds(200, 100, 150, 30);
 
+        // For Password
         labelpassword = new JLabel("Password:");
         labelpassword.setBounds(50, 150, 100, 30);
         passwordfield = new JPasswordField();
-        passwordfield.setBounds(200, 150, 100, 30);
+        passwordfield.setBounds(200, 150, 150, 30);
 
-        btninsert = new JButton("Login");
-        btninsert.setBounds(150, 200, 100, 30);
+        // For Button
+        btninsert = new JButton("Insert");
+        btninsert.setBounds(200, 200, 100, 30);
 
+        // Add all components to JFrame
         f.add(labelid);
         f.add(textfieldid);
         f.add(labelusername);
@@ -36,20 +42,48 @@ public class FormOne {
         f.add(passwordfield);
         f.add(btninsert);
 
-        btninsert.addActionListener(e-> {
+        // Database Connection
+        try {
+            // Step 1: Register the driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-            int id = Integer.parseInt(textfieldid.getText());
-            String  username =  textfieldusername.getText();
-            String pss = passwordfield.getText();
+            // Step 2: Create the connection
+            String url = "jdbc:mysql://localhost:3306/jdbc";
+            String username = "root";
+            String pass = "";
+            Connection con = DriverManager.getConnection(url, username, pass);
 
-            System.out.println("ID: "+id);
-            System.out.println("Username: " +username);
-            System.out.println("Password: " +pss);
-            
+            // Handle the event for the button btninsert
+            btninsert.addActionListener(e -> {
+                try {
+                    int id = Integer.parseInt(textfieldid.getText());
+                    String name = textfieldusername.getText();
+                    String Password = new String(passwordfield.getPassword());
 
-        });
+                    // Insert Query
+                    String insertQuery = "INSERT INTO employee(id, username, password) VALUES(?,?,?)";
+                    PreparedStatement pstmt = con.prepareStatement(insertQuery);
+                    pstmt.setInt(1, id);
+                    pstmt.setString(2, name);
+                    pstmt.setString(3, Password);
 
-        f.setSize(400, 400);
+                    // Execute the prepared statement
+                    int rowsInserted = pstmt.executeUpdate();
+                    if (rowsInserted > 0) {
+                        System.out.println("A new employee was inserted successfully!");
+                    }
+
+                } catch (Exception ex) {
+                    System.out.println(e);
+                }
+            });
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        // Set the size of the frame
+        f.setSize(1000, 1000);
         f.setLayout(null);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
