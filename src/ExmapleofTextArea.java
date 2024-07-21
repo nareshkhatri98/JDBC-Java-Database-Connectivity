f6aadda821b9e865b59b7e7c07e6bf4af70054ec
Naresh Khatri <nareshkhatri9810@gmail.com>
2024-07-21 19:12:17 +0545

    JtextArea

--- src/ExmapleofTextArea.java
+++ src/ExmapleofTextArea.java
@@ -1,2 +1,18 @@
+import  javax.swing.*;
 public class ExmapleofTextArea {
+    public  ExmapleofTextArea(){
+        JFrame f = new JFrame("Example of  JTextArea");
+        JTextArea A;
+        A = new JTextArea("Enter the text");
+        A.setBounds(50, 100, 100 ,30);
+        f.add(A);
+        f.setSize(400,300);
+        f.setVisible(true);
+        f.setLayout(null);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+    public static void main(String[] args) {
+        new ExmapleofTextArea();
+    }
 }
